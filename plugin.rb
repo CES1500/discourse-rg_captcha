36a651067249b5f9c7933c40288d4b539380adad
CES1500 <1836717285@qq.com>
2024-03-16 20:35:16 +0800

    first commi

--- plugin.rb
+++ plugin.rb
@@ -1,24 +1,24 @@
 # frozen_string_literal: true
 
-# name: discourse-hCaptcha
-# about: hCaptcha support for Discourse
+# name: discourse-rg_captcha
+# about: rg_captcha support for Discourse
 # version: 0.0.1
 # authors: Discourse
-# url: https://github.com/discourse/discourse-hCaptcha
+# url: https://github.com/discourse/discourse-rg_captcha
 # required_version: 2.7.0
 
-enabled_site_setting :discourse_hCaptcha_enabled
+enabled_site_setting :discourse_rg_captcha_enabled
 
-extend_content_security_policy(script_src: %w[https://hcaptcha.com])
+extend_content_security_policy(script_src: %w[https://rg_captcha.com])
 
-module ::DiscourseHCaptcha
-  PLUGIN_NAME = "discourse-hCaptcha"
+module ::Discourserg_captcha
+  PLUGIN_NAME = "discourse-rg_captcha"
 end
 
 require_relative "lib/discourse_h_captcha/engine"
 
 after_initialize do
   reloadable_patch do
-    UsersController.class_eval { include DiscourseHCaptcha::CreateUsersControllerPatch }
+    UsersController.class_eval { include Discourserg_captcha::CreateUsersControllerPatch }
   end
 end
